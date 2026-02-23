e838975cf5f98d3bdfd2930c3cae5009100552fe
Baskaran <umamaheswari.baskaran@capgemini.com>
2026-02-23 17:58:26 +0530

    edited program.cs

--- EmployeeManagement.API/Program.cs
+++ EmployeeManagement.API/Program.cs
@@ -85,11 +85,10 @@ builder.Services.AddAuthentication(u =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 
 app.UseAuthentication();
